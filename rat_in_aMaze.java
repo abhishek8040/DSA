c948339a6803f3b20e1c01e736c81b2014d5e572
Abhishek8040 <abhishekdubey8040@gmail.com>
2025-09-15 19:03:53 +0530

    rat

--- rat_in_aMaze.java
+++ rat_in_aMaze.java
@@ -4,6 +4,8 @@ public class rat_in_aMaze {
         int sc = 1, ec = 3;     // starting col = 1. // ending col(total col) = 3
         int count = maze(sr,sc,er,ec);
         System.out.println("Total ways: "+count);
+        System.out.println("Total Paths are:");
+        path(sr, sc, er, ec,"");
     }
     private static int maze(int sr, int sc, int er, int ec){
         if(sr>er || sc>ec) return 0;
@@ -12,6 +14,17 @@ public class rat_in_aMaze {
         int rightways = maze(sr, sc+1, er, ec);
         int totalways = downways + rightways;
         return totalways;
+    }
+    private static void path(int sr, int sc, int er, int ec, String s){
+        if(sr>er || sc>ec) return;
+        if(sr==er && sc==ec){
+            System.out.println(s);
+            return;
+        }
+        // go down
+        path(sr+1, sc, er, ec, s+"D");
+        // go right
+        path(sr, sc+1, er, ec, s+"R");
     }
     
 }
