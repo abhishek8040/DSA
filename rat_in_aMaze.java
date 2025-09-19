08b9bda8b4b0feaeceb50ae5c5e27f4d69931b92
Abhishek8040 <abhishekdubey8040@gmail.com>
2025-09-20 02:14:37 +0530

    Restricted Maze

--- rat_in_aMaze.java
+++ rat_in_aMaze.java
@@ -1,5 +1,7 @@
 public class rat_in_aMaze {
+    ////-----------------------------------------------------------------------//
     // // when only two direction is allowed : down and right
+    ////-----------------------------------------------------------------------//
     // public static void main(String[] args) {
     //     int sr=1, er = 3;       // starting row = 1. // ending row(total row) = 3
     //     int sc = 1, ec = 3;     // starting col = 1. // ending col(total col) = 3
@@ -27,41 +29,89 @@ public class rat_in_aMaze {
     //     // go right
     //     path(sr, sc+1, er, ec, s+"R");
     // }
-    // //when movement in all 4 directions is allowed
+
+    ////-----------------------------------------------------------------------//
+    
+    // //   when movement in all 4 directions is allowed
+
+    ////-----------------------------------------------------------------------//
+
+    // public static void main(String[] args) {
+    //     int rows= 3;
+    //     int cols= 3;
+
+    //     boolean isVisited[][] = new boolean[rows][cols];
+
+    //     System.out.println("All Possible paths are: ");
+    //     path(0,0,rows-1,cols-1,"",isVisited);
+
+    // }
+    // private static void path(int sr, int sc, int er, int ec, String s, boolean isVisited[][]){
+    //     if(sr<0 || sc <0) return;
+    //     if(sr>er || sc>ec) return;
+    //     if(isVisited[sr][sc]== true) return;
+    //     if(sr==er && sc==ec){
+    //         System.out.println(s);
+    //         return;
+    //     }
+    //     // mark visited
+    //     isVisited[sr][sc]= true;
+    //     // go right
+    //     path(sr, sc+1, er, ec, s+"R", isVisited);
+    //     // go down
+    //     path(sr+1, sc, er, ec, s+"D", isVisited);
+    //     // go left
+    //     path(sr, sc-1, er, ec, s+"L", isVisited);
+    //     //go up
+    //     path(sr-1, sc, er, ec, s+"U", isVisited);
+
+    //     //backtracking
+
+    //     isVisited[sr][sc] = false;
+    // }
+
+    //-----------------------------------------------------------------------//
+    //      Restrictions in maze (can not visit some boxes)
+    //-----------------------------------------------------------------------//
 
 
-    public static void main(String[] args) {
-        int rows= 3;
-        int cols= 3;
-
-        boolean isVisited[][] = new boolean[rows][cols];
-
-        System.out.println("All Possible paths are: ");
-        path(0,0,rows-1,cols-1,"",isVisited);
-
-    }
-    private static void path(int sr, int sc, int er, int ec, String s, boolean isVisited[][]){
-        if(sr<0 || sc <0) return;
-        if(sr>er || sc>ec) return;
-        if(isVisited[sr][sc]== true) return;
+    public static void pathofRestrictedMaze(int sr,int sc, int er, int ec , String s,int maze[][]){
+        if(sr<0 || sc<0) return;
+        if(sr>er || sc > ec) return;
         if(sr==er && sc==ec){
             System.out.println(s);
             return;
         }
-        // mark visited
-        isVisited[sr][sc]= true;
-        // go right
-        path(sr, sc+1, er, ec, s+"R", isVisited);
-        // go down
-        path(sr+1, sc, er, ec, s+"D", isVisited);
-        // go left
-        path(sr, sc-1, er, ec, s+"L", isVisited);
-        //go up
-        path(sr-1, sc, er, ec, s+"U", isVisited);
+        if(maze[sr][sc] == 0) return;  // Blocked or Restricted path
+        if(maze[sr][sc] == -1) return;  // this was already visited
+        // check
+        maze[sr][sc] = -1;
 
-        //backtracking
+        // Go Right
+        pathofRestrictedMaze(sr, sc+1, er, ec, s+"R", maze);
 
-        isVisited[sr][sc] = false;
+        // Go Down
+        pathofRestrictedMaze(sr+1, sc, er, ec, s+"D", maze);
+        // Go Left 
+        pathofRestrictedMaze(sr, sc-1, er, ec, s+"L", maze);
+        // Go Up
+        pathofRestrictedMaze(sr-1, sc, er, ec, s+"U", maze);
+
+        // backtracking
+        maze[sr][sc] = 1;
     }
+
+    public static void main(String[] args) {
+        int rows= 3, cols = 4;
+        int maze[][] = {{1,0,1,1},
+                        {1,1,1,1},
+                        {1,1,0,1}
+
+                        };
+        pathofRestrictedMaze(0,0, rows-1, cols-1,"", maze);
+
+    }
+
+
 
 }
